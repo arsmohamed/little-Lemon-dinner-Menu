248578be639b7bb3c36cc7b6501e548f1c0ec5dd
Mero "Amr R. Mohamed <arsmohamed@outlook.com>
2023-03-15 07:37:53 -0230

    initate the final project in advance ios

---
+++ little Lemon dinner Menu/little_Lemon_dinner_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_Lemon_dinner_MenuApp.swift
+//  little Lemon dinner Menu
+//
+//  Created by Amr Mohamed on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct little_Lemon_dinner_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
