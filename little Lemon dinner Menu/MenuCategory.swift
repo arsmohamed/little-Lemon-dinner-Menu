1e515a8d1cd81796b1f099a1aeb3f6e491085067
Mero "Amr R. Mohamed <arsmohamed@outlook.com>
2023-03-17 18:14:52 -0230

    done with task 3 and now will look in the next task

---
+++ little Lemon dinner Menu/MenuCategory.swift
@@ -0,0 +1,14 @@
+//
+//  MenuCategory.swift
+//  little Lemon dinner Menu
+//
+//  Created by Amr Mohamed on 2023-03-17.
+//
+
+import Foundation
+
+enum MenuCategory : String {
+    case Food = "Food"
+    case Drink = "Drink"
+    case Dessert = "Dessert"
+}
