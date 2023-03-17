54f587b6e1f3ea316585ecc82a2387beb227a770
Mero "Amr R. Mohamed <arsmohamed@outlook.com>
2023-03-17 16:54:08 -0230

    done with second task and moving to the 3rd one

--- little Lemon dinner Menu/MenuItemsOptionView.swift
+++ little Lemon dinner Menu/MenuItemsOptionView.swift
@@ -7,28 +7,47 @@
 
 import SwiftUI
 
+enum Categories: String,CaseIterable {
+    case Food = "Food"
+    case Drink = "Drink"
+    case Dessert = "Dessert"
+}
+enum sorting: String, CaseIterable {
+    case MostPopular = "Most Popular"
+    case price = "Price $-$$$"
+    case alpha = "A-Z"
+}
 struct MenuItemsOptionView: View {
+    
     var body: some View {
-        VStack {
+        VStack (alignment: .leading){
             Text("Filter")
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: 50,
-                    alignment: .topLeading)
                 .fontWeight(.bold)
                 .font(.system(size: 35))
-            Text("selected categories")
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: 20,
-                    alignment: .topLeading)
-            Text("Sort By")
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: .infinity,
-                    alignment: .topLeading)
+            
+            Text("SELECTED CATEGORIES")
+                .padding([.top, .bottom],5)
+                .fontWeight(.thin)
+                .font(.system(size: 13))
+            Text( Categories.Food.rawValue).padding([.bottom],5)
+            Text( Categories.Drink.rawValue).padding([.bottom],5)
+            Text( Categories.Dessert.rawValue).padding([.bottom],5)
+            
+            Text("SORT BY")
+                .padding([.top, .bottom],5)
+                .fontWeight(.thin)
+                .font(.system(size: 13))
+            Text( sorting.MostPopular.rawValue).padding([.bottom],5)
+            Text( sorting.price.rawValue).padding([.bottom],5)
+            Text( sorting.alpha.rawValue).padding([.bottom],5)
         }
         .padding()
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity,
+            alignment: .topLeading
+        )
+        
     }
 }
 
